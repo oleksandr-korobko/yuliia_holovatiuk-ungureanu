3c1ac51fdf424b33d0606ad45ca6de0fd84cf078
Oleksandr Korobko <oleksandr.korobko@gmail.com>
2025-03-12 12:00:37 +0200

    changed header menu names, changed page.tsx text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -69,35 +69,25 @@ export default function About() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            I’m Spencer Sharp. I live in New York City, where I design the
-            future.
+            I am a thinker, changemaker, and multimedia artist exploring themes
+            of war, displacement, and resilience.
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
-              I’ve loved making things for as long as I can remember, and wrote
-              my first program when I was 6 years old, just two weeks after my
-              mom brought home the brand new Macintosh LC 550 that I taught
-              myself to type on.
+              Before dedicating myself fully to art, I founded Ukraine’s Energy
+              Efficiency Fund, leading national initiatives in housing and
+              sustainability policy.
             </p>
             <p>
-              The only thing I loved more than computers as a kid was space.
-              When I was 8, I climbed the 40-foot oak tree at the back of our
-              yard while wearing my older sister’s motorcycle helmet, counted
-              down from three, and jumped — hoping the tree was tall enough that
-              with just a bit of momentum I’d be able to get to orbit.
+              This experience in systemic transformation informs my artistic
+              practice—where I work with found objects, installation, and
+              archival materials to examine destruction, memory, and cultural
+              survival.
             </p>
             <p>
-              I spent the next few summers indoors working on a rocket design,
-              while I recovered from the multiple surgeries it took to fix my
-              badly broken legs. It took nine iterations, but when I was 15 I
-              sent my dad’s Blackberry into orbit and was able to transmit a
-              photo back down to our family computer from space.
-            </p>
-            <p>
-              Today, I’m the founder of Planetaria, where we’re working on
-              civilian space suits and manned shuttle kits you can assemble at
-              home so that the next generation of kids really <em>can</em> make
-              it to orbit — from the comfort of their own backyards.
+              My art exists at the intersection of history and the present,
+              using visual storytelling to challenge narratives and create
+              spaces for reflection.
             </p>
           </div>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -121,11 +121,12 @@ function MobileNavigation(
         </div>
         <nav className="mt-6">
           <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
+            <MobileNavItem href="/articles">Main</MobileNavItem> {/* посилання на сторінку */}
             <MobileNavItem href="/about">About</MobileNavItem>
-            <MobileNavItem href="/articles">Articles</MobileNavItem>
             <MobileNavItem href="/projects">Projects</MobileNavItem>
-            <MobileNavItem href="/speaking">Speaking</MobileNavItem>
-            <MobileNavItem href="/uses">Uses</MobileNavItem>
+            <MobileNavItem href="/speaking">Exhibitions & Events</MobileNavItem> {/* посилання на сторінку */}
+            {/* <MobileNavItem href="/blog">Blog</MobileNavItem> */}
+            <MobileNavItem href="/uses">Contacts</MobileNavItem> {/* посилання на сторінку */}
           </ul>
         </nav>
       </PopoverPanel>
@@ -166,11 +167,12 @@ function DesktopNavigation(props: React.ComponentPropsWithoutRef<'nav'>) {
   return (
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 ring-1 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+        <NavItem href="/articles">Main</NavItem> {/* посилання на сторінку */}
         <NavItem href="/about">About</NavItem>
-        <NavItem href="/articles">Articles</NavItem>
         <NavItem href="/projects">Projects</NavItem>
-        <NavItem href="/speaking">Speaking</NavItem>
-        <NavItem href="/uses">Uses</NavItem>
+        <NavItem href="/speaking">Exhibitions & Events</NavItem>{/* посилання на сторінку */}
+        {/* <NavItem href="/blog">Blog</NavItem> */}
+        <NavItem href="/uses">Contacts</NavItem> {/* посилання на сторінку */}
       </ul>
     </nav>
   )
